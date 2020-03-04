3761f58dbb19c5bccd4fc1ddef97ffa8da3df3f0
root <root@DESKTOP-05QE8D6.localdomain>
2020-03-04 10:54:02 +0800

    修正脚本...

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone -b dev https://github.com/Lao-Liu233/FZYZ_JK_Grades.git
 cd /root/FZYZ_JK_Grades
 pip3 install -r requirements.txt
 cd /root/FZYZ_JK_Grades/API
-bash /root/FZYZ_JK_Grades/runserver.sh
+nohup bash /root/FZYZ_JK_Grades/runserver.sh &
