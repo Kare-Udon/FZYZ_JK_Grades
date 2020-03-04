3328bff2bb9f4dc6c85b19c2b36fc05df3738a1b
root <root@DESKTOP-05QE8D6.localdomain>
2020-03-04 10:52:08 +0800

    修改 settings.py; 新增安装脚本。

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /root
+git clone -b dev https://github.com/Lao-Liu233/FZYZ_JK_Grades.git
+cd /root/FZYZ_JK_Grades
+pip3 install -r requirements.txt
+cd /root/FZYZ_JK_Grades/API
+bash /root/FZYZ_JK_Grades/runserver.sh
