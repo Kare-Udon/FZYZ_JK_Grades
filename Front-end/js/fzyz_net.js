3a206ff4615e192550e4cfab0ee480870270e0af
Lao-Liu233 <liuhong751113@gmail.com>
2020-03-09 18:48:26 +0800

    添加进度条，防止同学因为过于垃圾的服务器所带来的等待而感到无聊。

--- Front-end/js/fzyz_net.js
+++ Front-end/js/fzyz_net.js
@@ -18,7 +18,8 @@ $(document).ready(function () {
             data: $('#form').serialize(),
             success: function (data) { //【成功回调】
                 console.log("success");
-                $("#next2").attr("class","btn btn-success");
+                $("#progress1").fadeOut();
+                $("#next2").attr("class", "btn btn-success");
                 $("#next2").html("成功");
                 $("#card2").fadeIn();
                 $("#warning").remove();
@@ -29,12 +30,36 @@ $(document).ready(function () {
             },
             error: function (xhr, type) { //【失败回调】
                 console.log("error");
-                $("#next2").attr("class","btn btn-danger");
+                $("#progress1").fadeOut();
+                $("#next2").attr("class", "btn btn-danger");
                 $("#next2").html("请重试");
                 $("#card2").fadeOut();
                 $("#warning").append("<p style='color: red;'>账号/密码错误或服务器/网络问题,<br />若一直出现此问题请联系网站管理员。</p>");
             }
         });
+
+        $("#progress1").fadeIn();
+        setTimeout(timeout1, 1500)
+        setTimeout(timeout2, 4000)
+        setTimeout(timeout3, 6000)
+        setTimeout(timeout4, 10000)
+
+        function timeout1() {
+            $("#progress1_inside").attr("style", "width: 65%");
+            $("#progress1_inside").html("很多浏览器的进度条都是假的");
+        }
+        function timeout2() {
+            $("#progress1_inside").attr("style", "width: 15%");
+            $("#progress1_inside").html("比如这样");
+        }
+        function timeout3() {
+            $("#progress1_inside").attr("style", "width: 85%");
+            $("#progress1_inside").html("只有少数浏览器是按照步骤显示进度");
+        }
+        function timeout4() {
+            $("#progress1_inside").attr("style", "width: 100%");
+            $("#progress1_inside").html("还没好吗？？？");
+        }
     });
 
     $("#submit").click(function () {
@@ -45,6 +70,7 @@ $(document).ready(function () {
             data: $('#form').serialize(),
             success: function (data) { //【成功回调】
                 console.log("success");
+                $("#progress2").fadeOut();
                 $("#tbody1").find("tr").remove();
                 $("#tbody1").append(data);
                 document.querySelectorAll('tr').forEach(
@@ -68,10 +94,33 @@ $(document).ready(function () {
             },
             error: function (xhr, type) { //【失败回调】
                 console.log("error");
-                $("#submit").attr("class","btn btn-danger");
-                $("#submit").attr("value","请重试");
+                $("#progress2").fadeOut();
+                $("#submit").attr("class", "btn btn-danger");
+                $("#submit").attr("value", "请重试");
                 $("#warning2").append("<br /><p style='color: red;'>服务器/网络问题，<br />若一直出现此问题请联系网站管理员。</p>");
             }
         });
+        $("#progress2").fadeIn();
+        setTimeout(timeout1, 1500)
+        setTimeout(timeout2, 4000)
+        setTimeout(timeout3, 6000)
+        setTimeout(timeout4, 10000)
+
+        function timeout1() {
+            $("#progress2_inside").attr("style", "width: 65%");
+            $("#progress2_inside").html("很多浏览器的进度条都是假的");
+        }
+        function timeout2() {
+            $("#progress2_inside").attr("style", "width: 15%");
+            $("#progress2_inside").html("比如这样");
+        }
+        function timeout3() {
+            $("#progress2_inside").attr("style", "width: 85%");
+            $("#progress2_inside").html("只有少数浏览器是按照步骤显示进度的");
+        }
+        function timeout4() {
+            $("#progress2_inside").attr("style", "width: 100%");
+            $("#progress2_inside").html("还没好吗？？？");
+        }
     });
 });
--- Front-end/js/jk.js
+++ Front-end/js/jk.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
             data: $('#form').serialize(),
             success: function (data) { //【成功回调】
                 console.log("success");
+                $("#progress1").fadeOut();
                 $("#submit").attr("class","btn btn-primary");
                 $("#submit").attr("value","提交查询");
                 $("#table1").append(data);
@@ -20,10 +21,34 @@ $(document).ready(function () {
             },
             error: function (xhr, type) { //【失败回调】
                 console.log("error");
+                $("#progress1").fadeOut();
                 $("#submit").attr("class","btn btn-danger");
                 $("#submit").attr("value","请重试");
                 $("#warning").append("<br /><p style='color: red;'>账号/密码错误或服务器/网络问题,<br />若一直出现此问题请联系网站管理员。</p>");
             }
         });
+
+        $("#progress1").fadeIn();
+        setTimeout(timeout1, 1500)
+        setTimeout(timeout2, 4000)
+        setTimeout(timeout3, 6000)
+        setTimeout(timeout4, 10000)
+
+        function timeout1() {
+            $("#progress1_inside").attr("style", "width: 65%");
+            $("#progress1_inside").html("很多浏览器的进度条都是假的");
+        }
+        function timeout2() {
+            $("#progress1_inside").attr("style", "width: 15%");
+            $("#progress1_inside").html("比如这样");
+        }
+        function timeout3() {
+            $("#progress1_inside").attr("style", "width: 85%");
+            $("#progress1_inside").html("只有少数浏览器是按照步骤显示进度");
+        }
+        function timeout4() {
+            $("#progress1_inside").attr("style", "width: 100%");
+            $("#progress1_inside").html("还没好吗？？？");
+        }
     });
 });
